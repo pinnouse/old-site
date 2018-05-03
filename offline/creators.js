98673288a52499f6e4986426ec24cb6460314188
pinnouse <cupofpho@gmail.com>
2018-05-03 00:33:50 -0400

    Some updates to the offline page

--- offline/creators.js
+++ offline/creators.js
@@ -1,35 +1,36 @@
-var $animation_elements = $('.creator');
-            var $window = $('#creators');
-            var $pages = $('.page');
+$(() => {
+    var $animation_elements = $('.creator');
+    var $window = $('#creators');
 
-            function checkView(resize = false) {
-                var window_height = $window.height();
-                var window_top_position = $window.scrollTop();
-                var bottom_threshold = (window_top_position + (window_height * 0.7));
+    function checkView(resize = false) {
+        var window_height = $window.height();
+        var window_top_position = $window.scrollTop();
+        var bottom_threshold = (window_top_position + (window_height * 0.7));
 
-                $.each($animation_elements, function(index, value) {
-                    var $element = $(value);
-                    var element_top = $element.offset().top;
+        $.each($animation_elements, function(index, value) {
+            var $element = $(value);
+            var element_top = $element.offset().top;
 
-                    if (resize)
-                        element_top -= 100;
+            if (resize)
+                element_top -= 100;
 
-                    if (element_top <= window_height * 0.97) {
-                        $element.addClass('in-view');
-                    } else {
-                        $element.removeClass('in-view');
-                    }
-                });
+            if (element_top <= window_height * 0.97) {
+                $element.addClass('in-view');
+            } else {
+                $element.removeClass('in-view');
             }
+        });
+    }
 
-            window.addEventListener('scroll', function (event) {
-                if (event.target.id === 'creators') {
-                    checkView();
-                }
-            }, true);
+    window.addEventListener('scroll', function (event) {
+        if (event.target.id === 'creators') {
+            checkView();
+        }
+    }, true);
 
-            window.addEventListener('resize', function (event) {
-                checkView(true);
-            }, true);
+    window.addEventListener('resize', function (event) {
+        checkView(true);
+    }, true);
 
-            checkView(true);
+    checkView(true);
+});
--- offline/websocketConnection.js
+++ offline/websocketConnection.js
@@ -1,6 +1,142 @@
+var main = [
+    {
+        "name": "Based Yoona",
+        "twitch": "based_yoona",
+        "youtube": "user/xTheLastSpartanxx",
+        "twitter": "BasedYoona",
+        "instagram": "based_yoona",
+        "facebook": "BasedYoona"
+    },
+    {
+        "name": "Disguised Toast",
+        "twitch": "disguisedtoasths",
+        "youtube": "channel/UCUT8RoNBTJvwW1iErP6-b-A",
+        "twitter": "DisguisedToast",
+        "instagram": "thedisguisedtoast",
+        "facebook": "disguisedtoast"
+    },
+    {
+        "name": "Fed Myster",
+        "twitch": "fedmyster",
+        "youtube": "channel/UCOmXyEquWIIo1uAj_2LN4UA",
+        "twitter": "Fedmyster",
+        "instagram": "fedmyster",
+        "facebook": "Fedmyster"
+    },
+    {
+        "name": "LilyPichu",
+        "twitch": "lilypichu",
+        "youtube": "user/LilyPichu",
+        "twitter": "LilyPichu",
+        "instagram": "lilypichu",
+        "facebook": "lilypichu"
+    },
+    {
+        "name": "Pokimane",
+        "twitch": "pokimane",
+        "youtube": "user/pokimane",
+        "twitter": "pokimanelol",
+        "instagram": "pokimanelol",
+        "facebook": "pokimane"
+    },
+    {
+        "name": "Scarra",
+        "twitch": "scarra",
+        "youtube": "user/scarraofficial",
+        "twitter": "scarra",
+        "instagram": "scarraa",
+        "facebook": "scarraofficial"
+    }
+];
+
+var side = [
+    {
+        "name": "Chris Chan",
+        "twitch": "chrischantor",
+        "twitter": "ChrisChanTO",
+        "instagram": "chrischantor",
+        "facebook": "OfflineChris"
+    },
+    {
+        "name": "Pecca",
+        "twitch": "peccapecca",
+        "twitter": "peccapecca",
+        "instagram": "peccapeccapecca",
+        "facebook": "peccapeccapecca"
+    },
+    {
+        "name": "Sleightly Musical",
+        "twitch": "sleightlymusical",
+        "youtube": "user/sleightlymusical",
+        "twitter": "THEalbertchang",
+        "instagram": "sleightlymusical",
+        "facebook": "sleightlymusical"
+    },
+    {
+        "name": "TheeMarkZ",
+        "twitch": "theemarkz",
+        "youtube": "channel/UCU74OVWGSmJqR1g6y-tgUHQ",
+        "twitter": "TheeMarkZ",
+        "instagram": "theemarkz",
+        "facebook": "theemarkz"
+    },
+    {
+        "name": "Xell",
+        "twitch": "xell",
+        "youtube": "user/OfficialXell",
+        "twitter": "XellTweets",
+        "instagram": "xellgrams",
+        "facebook": "LoLXell"
+    }
+];
+
 $(function() {
     window.WebSocket = window.WebSocket || window.MozWebSocket;
+    
+    main.forEach((value, index) => {
+        var links = "";
+        if (value.twitch)
+        links += `<li><h3><a class='twitch' href='https://twitch.tv/${value.twitch}' target='_blank'>Twitch</a></h3></li>`
+        if (value.youtube)
+        links += `<li><h3><a class='youtube' href='https://youtube.com/${value.twitch}' target='_blank'>YouTube</a></h3></li>`
+        if (value.twitter)
+        links += `<li><h3><a class='twitter' href='https://twitter.com/${value.twitch}' target='_blank'>Twitter</a></h3></li>`
+        if (value.instagram)
+        links += `<li><h3><a class='instagram' href='https://instagram.com/${value.twitch}' target='_blank'>Instagram</a></h3></li>`
+        if (value.facebook)
+        links += `<li><h3><a class='facebook' href='https://facebook.com/${value.twitch}' target='_blank'>Facebook</a></h3></li>`
+        $("#main").append(
+            `<div class='creator bounce-up' data-streamer=${value.twitch}>`
+            + `<div class='image-adjust'><img src='https://twitter.com/${value.twitter}/profile_image?size=original' /></div>`
+            + `<h2>${value.name}</h2>`
+            + "<div class='streaming-border'><div class='streaming-indicator'></div></div>"
+            + `<ul>${links}</ul>`
+            + "</div>"
+        );
+    });
 
+    side.forEach((value, index) => {
+        var links = "";
+        if (value.twitch)
+        links += `<li><h3><a class='twitch' href='https://twitch.tv/${value.twitch}' target='_blank'>Twitch</a></h3></li>`
+        if (value.youtube)
+        links += `<li><h3><a class='youtube' href='https://youtube.com/${value.twitch}' target='_blank'>YouTube</a></h3></li>`
+        if (value.twitter)
+        links += `<li><h3><a class='twitter' href='https://twitter.com/${value.twitch}' target='_blank'>Twitter</a></h3></li>`
+        if (value.instagram)
+        links += `<li><h3><a class='instagram' href='https://instagram.com/${value.twitch}' target='_blank'>Instagram</a></h3></li>`
+        if (value.facebook)
+        links += `<li><h3><a class='facebook' href='https://facebook.com/${value.twitch}' target='_blank'>Facebook</a></h3></li>`
+        $("#side").append(
+            `<div class='creator bounce-up' data-streamer=${value.twitch}>`
+            + `<div class='image-adjust'><img src='https://twitter.com/${value.twitter}/profile_image?size=original' /></div>`
+            + `<h2>${value.name}</h2>`
+            + "<div class='streaming-border'><div class='streaming-indicator'></div></div>"
+            + `<ul>${links}</ul>`
+            + "</div>"
+        );
+    });
+    
     var connection = setupConnection();
 
     setInterval(function() {
@@ -21,19 +157,22 @@ function setupConnection() {
 
     connection.onmessage = function(message) {
         console.log((new Date) + ' message from websocket');
-        let presence = JSON.parse(message.data).streamers;
-        presence.forEach(function(streamer) {
-            $element = $(`div[data-streamer="${streamer.name}"]`);
-            if (streamer.online) {
-                $element.css("background", "rgb(181, 64, 215)");
-                if (!$element.hasClass('live'))
-                    $element.addClass('live');
-                $element.removeClass('offline');
+        let presence = JSON.parse(message.data).entertainers;
+        console.log(JSON.parse(message.data));
+        presence.forEach(function(entertainer) {
+            element = $(`div[data-streamer="${entertainer.name}"]`);
+            streamingIndicator = element.find(".streaming-indicator");
+            element.find("img").attr("src", entertainer.twitLink.replace("_normal", ''));
+            if (entertainer.online) {
+                streamingIndicator.css("background", "rgb(181, 64, 215)");
+                if (!streamingIndicator.hasClass('live'))
+                    streamingIndicator.addClass('live');
+                streamingIndicator.removeClass('offline');
             } else {
-                $element.css("background", "rgb(140, 140, 140)");
-                if (!$element.hasClass('offline'))
-                    $element.addClass('offline');
-                $element.removeClass('live');
+                streamingIndicator.css("background", "rgb(140, 140, 140)");
+                if (!streamingIndicator.hasClass('offline'))
+                    streamingIndicator.addClass('offline');
+                streamingIndicator.removeClass('live');
             }
         });
     }
